5ab6a814adb692d6c4f214e00488372441faa6d6
Artem Pushkar <artem.pushkar@globallogic.com>
2017-10-04 13:12:16 +0300

    - add circle ci config file

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo " Hello from bash script! "
+echo " Hello from bash script! "
+echo " Hello from bash script! "
+
+# ssh -v root@46.101.168.249 << EOF
+
+# echo '1. Updating sources'
+# cd /patch/to/your/repository/
+# git checkout --force master
+# git pull
+
+# echo "2. Restart apache"
+# sudo apache2ctl graceful
+
+#cd ;
+#
+#touch test.txt
+#
+#
+#echo 'Done!'
+#
+#EOF
