b3f00adf373b7924b548ad2b734cf199365c155e
Artem Pushkar <artem.pushkar@globallogic.com>
2017-10-10 02:42:12 +0300

    - add themes presentation modal window

--- public/src/app/components/theses/theses.js
+++ public/src/app/components/theses/theses.js
@@ -1,9 +1,32 @@
 import $ from 'jquery';
 import './theses.scss';
 
+$('#theses-modal').on('shown.bs.modal', function () {
+	$('#name').focus();
 
-// uncomment to open modal
-//$('#theses-modal').on('shown.bs.modal', function () {
-//	//$('#myInput').focus()
-//	console.log('Modal is open');
-//});
+	$("form.modal-content").submit(function (event) {
+		const name = $('#name').val();
+		const surname = $('#surname').val();
+		const position = $('#position').val();
+		const place = $('#place').val();
+		const city = $('#city').val();
+		const country = $('#country').val();
+		const email = $('#email').val();
+		const phone = $('#phone').val();
+		const theseName = $('#these-name').val();
+		const description = $('#description').val();
+
+		const params = {
+			name, surname, position, place, city, country, email, phone, theseName, description
+		};
+
+		$.ajax('', params).then(function () {
+
+		}).catch(err => {
+			console.log('err ', err);
+		});
+
+		event.preventDefault();
+		event.stopImmediatePropagation();
+	});
+});
