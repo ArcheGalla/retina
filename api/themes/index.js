d3af07d1b6a747edcd21a2df46a687883bc03a4b
Artem Pushkar <artem.pushkar@globallogic.com>
2017-10-29 02:58:30 +0300

    - fix upload themes

--- api/themes/index.js
+++ api/themes/index.js
@@ -1,8 +1,5 @@
-//const positionEnum = ['professor', 'doctor', 'docent', 'assistant', 'професор', 'доктор', 'доцент', 'асистент'];
-//const placeEnum = ['clinic', 'university', 'клініка', 'університет'];
 const { Validator } = require('express-json-validator-middleware');
 const envVariables = require('../../api/const/constant');
-const validator = new Validator({ allErrors: true });
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
@@ -13,43 +10,10 @@ const STATUS_CODES = require('http').STATUS_CODES;
 const dest = path.resolve(envVariables.THEME_STORAGE);
 const upload = multer({ dest });
 
-//city    :    "orativ"
-//country    :    "Ukraine"
-//email	:	"artempushkar@gmail.com"
-//name	:	"arche galla"
-//phone	:	"734183185"
-//place	:	"clinic"
-//position	:	"doctor"
-//surname	:	"galla"
-//theme	:	"C:\fakepath\theme.txt"
-//topic	:	"asdasd"
-const ThemeSchemas = {
-	type: 'object',
-	required: ['name', 'surname', 'country', 'email', 'topic', 'city', 'position', 'place', 'phone'],
-	properties: {
-		name: { type: 'string' },
-		surname: { type: 'string' },
-		country: { type: 'string' },
-		email: { type: 'string' },
-		topic: { type: 'string' },
-		city: { type: 'string' },
-		position: { type: 'string' },
-		place: { type: 'string' },
-		phone: { type: 'string' },
-		//theme: { }
-	}
-};
+router.post('/', upload.single('theme'), function (req, res) {
+	console.log('req.file ', req.file);
+	console.log('req.body ', req.body);
 
-function normalize(field) {
-	return field.trim().toLowerCase();
-}
-
-function validateMiddleware(req, res, next) {
-	next();
-}
-
-//router.post('/', upload.single('theme'), validator.validate({ body: ThemeSchemas }), function (req, res) {
-router.post('/', upload.single('theme'), validateMiddleware, function (req, res) {
 	const { file } = req;
 	let locale;
 
@@ -60,6 +24,8 @@ router.post('/', upload.single('theme'), validateMiddleware, function (req, res)
 	}
 
 	const theme = req.body;
+	console.log('file',file);
+
 	const { originalname } = file;
 
 	// lower case all fields
--- public/src/app/components/theses/theses.js
+++ public/src/app/components/theses/theses.js
@@ -1,36 +1,41 @@
 import $ from 'jquery';
 import './theses.scss';
 
-$('#theses-modal').on('shown.bs.modal', function () {
-	$('#name').focus();
 
-	$("form.modal-content").submit(function (event) {
-		const submit = $('#theme-submit');
-		const alertSuccess = $('#alert-success');
-		const alertDanger = $('#alert-danger');
+$("#theses-modal form").submit(function (event) {
+	const submit = $('#theme-submit');
+	const alertSuccess = $('#alert-success');
+	const alertDanger = $('#alert-danger');
 
-		submit.attr('disabled', true);
+	submit.attr('disabled', true);
 
-		window
-			.fetch('/api/themes', {
-				method: 'POST',
-				body: new FormData(document.querySelector('form.modal-content'))
-			})
-			.then((response) => {
-				if (response.ok) {
-					alertSuccess.css({ display: 'block' });
-					setTimeout(() => alertSuccess.css({ display: 'none' }), 5500);
-					submit.attr('disabled', false);
-					$("form.modal-content")[0].reset();
-				} else throw new Error('Filed to submit theme');
-			})
-			.catch(() => {
+	window
+		.fetch('/api/themes', {
+			method: 'POST',
+			body: new FormData(document.querySelector('#theses-modal form'))
+		})
+		.then((response) => {
+			if (response.ok) {
+				alertSuccess.css({ display: 'block' });
+				setTimeout(() => alertSuccess.css({ display: 'none' }), 5500);
 				submit.attr('disabled', false);
-				alertDanger.css({ display: 'block' });
-				setTimeout(() => alertDanger.css({ display: 'none' }), 2500);
-			});
+				document.querySelector('#theses-modal form').reset();
+			} else throw new Error('Filed to submit theme');
+		})
+		.catch(() => {
+			submit.attr('disabled', false);
+			alertDanger.css({ display: 'block' });
+			setTimeout(() => alertDanger.css({ display: 'none' }), 2500);
+		});
 
-		event.preventDefault();
-		event.stopImmediatePropagation();
-	});
+	event.preventDefault();
+	event.stopImmediatePropagation();
 });
+
+$('#theses-modal')
+	.on('shown.bs.modal', function () {
+		$('#name').focus();
+	})
+	.on('hide.bs.modal', function () {
+		document.querySelector('#theses-modal form').reset();
+	});
