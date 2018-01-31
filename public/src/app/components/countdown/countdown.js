ffe03c3934db2f732b65b4dc83ba99c061d4119d
archegalla <artempushkar@gmail.com>
2018-01-31 22:50:32 +0200

    add package-lock file

--- public/src/app/components/countdown/countdown.js
+++ public/src/app/components/countdown/countdown.js
@@ -6,12 +6,11 @@ const start = Math.floor((new Date("1/7/2017")).getTime() / 1000);
 const now = Math.floor((new Date).getTime() / 1000);
 
 const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-// const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 if (width > 480) {
-	$(".countdown").final_countdown({
-		'start': start,
-		'end': end,
-		'now': now
-	}, () => console.log("finish call back"));
+    $(".countdown").final_countdown({
+        'start': start,
+        'end': end,
+        'now': now
+    });
 }
